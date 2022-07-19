8a630d9044add4d95a06a391c6dc563e0bfd0e22
Christopher Dearie <cdearie@gmail.com>
2022-07-19 18:04:07 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-west-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "dearie-terraform"
+    key    = "tfstate/us-west-2/terraform-galaxy/prod"
+    region = "us-west-2"
+  }
+}
